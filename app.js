e59cf3c763f3a52f455dbefebdb374738430b044
NaeemShaikh <kothiya.shabnam@yahoo.com>
2015-09-19 11:09:05 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,14 +10,14 @@
     function config($routeProvider, $locationProvider) {
         $routeProvider
             .when('/', {
-                controller: 'HomeController',
-                templateUrl: 'home/home.view.html',
+                controller: 'Controller',
+                templateUrl: 'index'.html',
                 controllerAs: 'vm'
             })
 
             .when('/login', {
-                controller: 'LoginController',
-                templateUrl: 'login/login.view.html',
+                controller: 'Controller',
+                templateUrl: 'index.html',
                 controllerAs: 'vm'
             })
 
